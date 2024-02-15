3cc4b844e57ec98810df055deb855c3e4944e2d7
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-02-14 21:13:28 -0300

    feat: working with physics!

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -7,7 +7,7 @@ enum States { IDLE, WALK, RUN }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Logo.rotation_degrees = 45
+	pass
 	# this is the same as absolute path
 	# $Sprite2D/Spri2D2/Sprite2D3/Target
 	# Sprite2D
@@ -17,16 +17,14 @@ func _ready():
 
 	# after adding in the editor access with unique name
 	# the path is more simple, addressing the node directly
-	%Target  # this is the same as $Sprite2D/Spri2D2/Sprite2D3/Target
-	print(%Target)
+	# %Target  # this is the same as $Sprite2D/Spri2D2/Sprite2D3/Target
+	# print(%Target)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	$Logo.rotation_degrees += 10 * delta
-
-	if $Logo.position.x == 1000:
-		$Logo.position.x = 0
+func _process(_delta):
+	# Add your code here
+	pass
 
 
 func test_function():
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,17 +1,18 @@
-extends Node2D
+extends CharacterBody2D
 
 var speed = 500
 
-func _process(delta):
-  #input  
-  var direction = Input.get_vector("left", "right", "up", "down")
-	
-  position += direction * speed * delta 
 
-  # laser shooting
-  if Input.is_action_just_pressed("primary action"):
-   print("pew pew")
+func _process(_delta):
+	#input
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()  # automatic uses delta
 
-  # grenade throwing
-  if Input.is_action_just_pressed("secondary action"):
-   print("boom")
+	# laser shooting
+	if Input.is_action_just_pressed("primary action"):
+		print("pew pew")
+
+	# grenade throwing
+	if Input.is_action_just_pressed("secondary action"):
+		print("boom aaaaaaaaa")
